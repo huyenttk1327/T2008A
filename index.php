08799ee0aec21755bf682cfd61ac7142f4484de2
Huyen <totoro7991@gamil.com>
2020-10-07 16:17:17 +0700

    bai tap

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Bai 1: Huong dan hoc HTML</title>
+	<meta charset="utf-8">
+	<!-- luu css -->
+	<!-- luu js -->
+</head>
+<body style="background-color: #90a8d6; background-image: url('https://m.media-amazon.com/images/M/MV5BMjM2OTkyNTY3N15BMl5BanBnXkFtZTgwNzgzNDc2NjE@._V1_CR132,0,761,428_AL_UY268_CR82,0,477,268_AL_.jpg'); background-repeat: no-repeat; background-size: cover; color: white">
+	<h1> title="Xin Chao H1">H1</h1>
